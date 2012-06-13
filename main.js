d5653ba864d471ffc238ee0dd48385d063aaedce
Matthew DuVall <mduvall89@gmail.com>
2012-06-13 14:18:28 -0700

    Add blog and additional styling

--- main.js
+++ main.js
@@ -13,8 +13,26 @@ var highlightImagesOnHover = function() {
   }); 
 };
 
+var highlightBlogOnHover = function() {
+  $("#blog-link").bind({
+    "mouseover": function() {
+      $(this).animate({
+        opacity: 1.0,
+      }, 250, function() {});
+      $(this).css("text-decoration", "underline");
+    },
+    "mouseout": function() {
+      $(this).animate({
+        opacity: 0.8,
+      }, 250, function() {});
+      $(this).css("text-decoration", "none");
+    }
+  });
+}
+
 var loadFunctions = function() {
   highlightImagesOnHover();
+  highlightBlogOnHover();
 };
 
 $(document).ready(function() {
